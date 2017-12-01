54cb57bc389bd814ce3a3abc84b98778798d6ebc
alcasa04 <casadotrapote98@gmail.com>
2017-12-01 12:39:07 +0100

    Nada5

--- plantilla-juego/src/js/main.js
+++ plantilla-juego/src/js/main.js
@@ -5,7 +5,7 @@ var PlayScene = require('./play_scene.js');
 
 var BootScene = {
   preload: function () {
-	  this.game.load.baseURL = 'http://alcasa04.github.io/PVLI/src/index.html';
+	  this.game.load.baseURL = 'http://alcasa04.github.io/PVLI';
 	  this.game.load.crossOrigin = 'anonymous';
     // load here assets required for the loading screen
     this.game.load.image('preloader_bar', 'images/preloader_bar.png');
