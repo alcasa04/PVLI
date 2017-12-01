fa84c9c21f8a70a3adfbbe8e1bc8ffab2552ff9b
alcasa04 <casadotrapote98@gmail.com>
2017-12-01 02:00:16 +0100

    Revert "Merge branch 'IA-y-comportamientos'"
    This reverts commit 6afa6580f07c92b0f0b29ccb99bc1a261bbe60d3, reversing
    changes made to 58de3b4ae6474798f37d66c2d0d4e96c3a58269b.

--- plantilla-juego/src/js/play_scene.js
+++ plantilla-juego/src/js/play_scene.js
@@ -34,10 +34,15 @@ var PlayScene = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 	prota.width= 50;
 	prota.height = 75;
 	prota.anchor.set(.5,0);
+=======
+	prota.width= 50;
+	prota.height = 75;
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
 	prota.width= 50;
 	prota.height = 75;
@@ -60,6 +65,7 @@ var PlayScene = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	enemigo.anchor.set(0.5, 0.5);
 	this.game.physics.arcade.enable(enemigo);
 	enemigo.body.collideWorldBounds = false;
@@ -76,6 +82,9 @@ var PlayScene = {
 =======
 	this.game.physics.arcade.enable(enemigo);
 >>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
+=======
+	this.game.physics.arcade.enable(enemigo);
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 	enemigo.body.colliderWorldBounds = true;
 	cabeza = enemigo.addChild(this.game.add.sprite(0, -enemigo.height*3, 'cabeza'));
 	cabeza.height = cabeza.height*0.3;
@@ -83,6 +92,9 @@ var PlayScene = {
 	cabeza.body.colliderWorldBounds = true;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
+=======
 >>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
 >>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
@@ -95,7 +107,11 @@ var PlayScene = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	sueloNormal.body.immovable = true;
+=======
+	sueloNormal.body.inmovable = true;
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
 	sueloNormal.body.inmovable = true;
 >>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
@@ -116,8 +132,13 @@ var PlayScene = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	  var choque2 = this.game.physics.arcade.collide(enemigo, sueloNormal);
 	  var damageCabeza = this.game.physics.arcade.overlap(prota, cabeza);
+=======
+	  var damageCabeza = this.game.physics.arcade.overlap(prota, cabeza);
+	  
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
 	  var damageCabeza = this.game.physics.arcade.overlap(prota, cabeza);
 	  
@@ -173,6 +194,7 @@ var PlayScene = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if(saltoEnemigo >= 35 && !flipFlop3 && this.game.rnd.integerInRange(0, 100) <= 4)
 	{
 		enemigo.body.velocity.y -= fuerzaEmpuje*1.5;
@@ -181,6 +203,8 @@ var PlayScene = {
 	}
 
 	
+=======
+>>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
 >>>>>>> parent of 6afa658... Merge branch 'IA-y-comportamientos'
 =======
